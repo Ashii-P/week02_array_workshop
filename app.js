78730ecc811c8b5db3dba3c34491aa1454108d31
Aishah Perks <byaishah.altaf@gmail.com>
2025-01-13 14:52:01 +0000

    Array workshop task

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("hello world!");
+let blogPosts = ["My first blog", "My second blog", "My third blog"];
+console.log(blogPosts);
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog";
+console.log(blogPosts[3]);
+
+const favouriteFoods = [
+  "Pizza",
+  "Pho",
+  "Turkish meat platter",
+  "mashed potatoe",
+];
+console.log(favouriteFoods);
+
+const favouriteColours = ["Pink", "Green", "Black", "Beige"];
+console.log(favouriteColours);
+
+const favouriteNumbers = [7, 2, 19, 20, 28];
+console.log(favouriteNumbers);
